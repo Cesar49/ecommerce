0da2277a8e4c54546ba578047035a13776b07f6b
Cesar Esqueda <cesaresquedadeveloper@gmail.com>
2024-02-10 11:38:59 -0400

    se hizo el crud de administradores

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Hash;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function list()
+    {
+        $data['getRecord'] = User::getAdmin();
+        $data['header_title'] = "Admin";
+        return view('admin.admin.list', $data);
+    }
+
+    public function add()
+    {
+        
+        $data['header_title'] = "Agregar Nuevo Administrador";
+        return view('admin.admin.add', $data);
+    }
+
+     public function insert(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->status = $request->status;
+        $user->is_admin = 1;
+        $user->save();
+
+        return redirect('admin/admin/list')->with('success', "Administrador creado satisfactoriamente");
+    }
+
+    public function edit($id)
+    {
+        $data['getRecord'] = User::getSingle($id);
+        $data['header_title'] = "Editar Administrador";
+        return view('admin.admin.edit', $data);
+    }
+
+    public function update($id, Request $request)
+    {
+        $user = User::getSingle($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if (!empty($request->password)) {
+        $user->password = Hash::make($request->password);             
+        }
+        $user->status = $request->status;
+        $user->is_admin = 1;
+        $user->save();
+
+        return redirect('admin/admin/list')->with('success', "Administrador actualizado satisfactoriamente");
+    }
+
+    public function delete($id)
+    {
+        $user = User::getSingle($id);
+        $user->is_delete = 1;
+        $user->save();
+        return redirect()->back()->with('success', "Administrador borrado satisfactoriamente");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\AdminController;
 
 
 
@@ -13,15 +14,16 @@ Route::get('admin/logout', [AuthController::class, 'logout_admin']);
 Route::group(['middleware' => 'admin'], function () {
 
     Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
-    /*Route::get('admin/dashboard', function () {
-        return view('admin.dashboard');
-    });*/
+    Route::get('admin/admin/list', [AdminController::class, 'list']);
+    Route::get('admin/admin/add', [AdminController::class, 'add']);
+    Route::post('admin/admin/add', [AdminController::class, 'insert']);
+
+    Route::get('admin/admin/edit/{id}', [AdminController::class, 'edit']);
+    Route::post('admin/admin/edit/{id}', [AdminController::class, 'update']);
+    Route::get('admin/admin/delete/{id}', [AdminController::class, 'delete']);
+    
 
 
-    Route::get('admin/admin/list', function () {
-        $data['header_title'] = "Admin";
-        return view('admin.admin.list', $data);
-    });
 
 });
 
